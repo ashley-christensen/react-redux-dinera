bf5732f6ea8c64e1d381a7d434c61f5d2fe0b41a
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-28 13:17:18 -0700

    promos

--- src/app/shared/PROMOTIONS.js
+++ src/app/shared/PROMOTIONS.js
@@ -1,9 +1,6 @@
 import romanticImg from '../assets/img/romance.jpg';
-//Photo by Jonny Caspari on Unsplash
 import picnicImg from '../assets/img/picnic.jpg';
-// Photo by LoboStudio Hamburg on Unsplash
 import vegetarianImg from '../assets/img/vegetarian.jpg';
-// Photo by Jonathan Gallegos on Unsplash
 import partyImg from '../assets/img/party.jpg';
 
 export const PROMOTIONS = [
