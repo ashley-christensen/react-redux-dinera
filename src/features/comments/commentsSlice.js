8e2d3f3de601dcc08095ae80792a400db2d24d66
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-29 16:21:51 -0700

    useDispatch on comments

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -7,10 +7,24 @@ const initialState = {
 
 const commentsSlice = createSlice({
  name: 'comments',
- initialState
+ initialState,
+ reducers: {
+  addComment: (state, action) => {
+   console.log('addcomment action.payload:', action.payload);
+   console.log('addcomment state.commentsArray:', state.commentsArray);
+   const newComment = {
+    id: state.commentsArray.length + 1,
+    //rating, author, commentText
+    ...action.payload
+   };
+   state.commentsArray.push(newComment);
+  }
+ }
 });
 
 export const commentsReducer = commentsSlice.reducer;
+
+export const { addComment } = commentsSlice.actions;
 
 export const selectCommentsByDineId = (dineId) => (state) => {
  return state.comments.commentsArray.filter((comment) => comment.dineId === parseInt(dineId));
